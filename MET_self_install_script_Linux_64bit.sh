392a7e2501cbf8fef18f1d3030538575156ebd1e
Will H <80460171+whatheway@users.noreply.github.com>
2022-05-28 11:49:47 -0500

    Add files via upload

--- MET_self_install_script_Linux_64bit.sh
+++ MET_self_install_script_Linux_64bit.sh
@@ -11,31 +11,31 @@ sudo apt-get install python-dev python3-dev emacs flex bison libpixman-1-dev lib
 pip3 install python-dateutil==2.8
 
 #Directory Listings
-mkdir $HOME/WRF-Hydro
-mkdir $HOME/WRF-Hydro/MET-10.1.2
-mkdir $HOME/WRF-Hydro/MET-10.1.2/Downloads
-mkdir $HOME/WRF-Hydro/METplus-4.1.2
-mkdir $HOME/WRF-Hydro/METplus-4.1.2/Downloads
+mkdir $HOME/WRF
+mkdir $HOME/WRF/MET-10.1.2
+mkdir $HOME/WRF/MET-10.1.2/Downloads
+mkdir $HOME/WRF/METplus-4.1.2
+mkdir $HOME/WRF/METplus-4.1.2/Downloads
 
 
 
 #Downloading MET and untarring files
-cd $HOME/WRF-Hydro/MET-10.1.2/Downloads
+#Note weblinks change often update as needed.
+cd $HOME/WRF/MET-10.1.2/Downloads
 wget https://raw.githubusercontent.com/dtcenter/MET/main_v10.1/scripts/installation/compile_MET_all.sh
 
 wget https://dtcenter.ucar.edu/dfiles/code/METplus/MET/installation/tar_files.tgz
 wget https://github.com/dtcenter/MET/releases/download/v10.1.2/met-10.1.2.20220516.tar.gz
 
-
-cp compile_MET_all.sh $HOME/WRF-Hydro/MET-10.1.2
-tar -xvzf tar_files.tgz -C $HOME/WRF-Hydro/MET-10.1.2
-cp met-10.1.2.20220516.tar.gz $HOME/WRF-Hydro/MET-10.1.2/tar_files
-cd $HOME/WRF-Hydro/MET-10.1.2
+cp compile_MET_all.sh $HOME/WRF/MET-10.1.2
+tar -xvzf tar_files.tgz -C $HOME/WRF/MET-10.1.2
+cp met-10.1.2.20220516.tar.gz $HOME/WRF/MET-10.1.2/tar_files
+cd $HOME/WRF/MET-10.1.1
 
 
 
 # Installation of Model Evaluation Tools
-cd $HOME/WRF-Hydro/MET-10.1.2
+cd $HOME/WRF/MET-10.1.2
 sed -i 's/-fno-second-underscore -fallow-argument-mismatch/-fno-second-underscore -Wno-argument-mismatch/g' compile_MET_all.sh
 
 
@@ -43,7 +43,7 @@ export FC=/usr/bin/gfortran
 export F77=/usr/bin/gfortran
 export F90=/usr/bin/gfortran
 export gcc_version=$(gcc -dumpfullversion)
-export TEST_BASE=$HOME/WRF-Hydro/MET-10.1.2                       
+export TEST_BASE=$HOME/WRF/MET-10.1.2                       
 export COMPILER=gnu_$gcc_version                 
 export MET_SUBDIR=${TEST_BASE}                   
 export MET_TARBALL=met-10.1.2.20220516.tar.gz    
@@ -58,10 +58,10 @@ export SET_D64BIT=FALSE
 chmod 775 compile_MET_all.sh
 ./compile_MET_all.sh 
 
-export PATH=$HOME/WRF-Hydro/MET-10.1.2/bin:$PATH            #Add MET executables to path
+export PATH=$HOME/WRF/MET-10.1.2/bin:$PATH            #Add MET executables to path
 
 cd 
-$HOME/WRFHYDRO-Coupled-5.2-install-script-linux-64bit/METplus_self_install_script_Linux_64bit.sh
+$HOME/WRF-4.4-install-script-linux-64bit/METplus_self_install_script_Linux_64bit.sh
 
 
 
--- METplus_self_install_script_Linux_64bit.sh
+++ METplus_self_install_script_Linux_64bit.sh
@@ -9,35 +9,35 @@ sudo apt-get upgrade
 
 
 #Directory Listings for Model Evaluation Tools (METplus
-mkdir $HOME/WRF-Hydro
-mkdir $HOME/WRF-Hydro/METplus-4.1.2
-mkdir $HOME/WRF-Hydro/METplus-4.1.2/Sample_Data
-mkdir $HOME/WRF-Hydro/METplus-4.1.2/Output
-mkdir $HOME/WRF-Hydro/METplus-4.1.2/Downloads
+mkdir $HOME/WRF
+mkdir $HOME/WRF/METplus-4.1.2
+mkdir $HOME/WRF/METplus-4.1.2/Sample_Data
+mkdir $HOME/WRF/METplus-4.1.2/Output
+mkdir $HOME/WRF/METplus-4.1.2/Downloads
 
 
 
 #Downloading METplus and untarring files
 
-cd $HOME/WRF-Hydro/METplus-4.1.2/Downloads
+cd $HOME/WRF/METplus-4.1.2/Downloads
 wget https://github.com/dtcenter/METplus/archive/refs/tags/v4.1.2.tar.gz
-tar -xvzf v4.1.2.tar.gz -C $HOME/WRF-Hydro
+tar -xvzf v4.1.2.tar.gz -C $HOME/WRF
 
 
 
 # Insatlllation of Model Evaluation Tools Plus
-cd $HOME/WRF-Hydro/METplus-4.1.2/parm/metplus_config
+cd $HOME/WRF/METplus-4.1.2/parm/metplus_config
 
-sed -i "s|MET_INSTALL_DIR = /path/to|MET_INSTALL_DIR = $HOME/WRF-Hydro/MET-10.1.2|" defaults.conf
-sed -i "s|INPUT_BASE = /path/to|INPUT_BASE = $HOME/WRF-Hydro/METplus-4.1.2/Sample_Data|" defaults.conf
-sed -i "s|OUTPUT_BASE = /path/to|OUTPUT_BASE = $HOME/WRF-Hydro/METplus-4.1.2/Output|" defaults.conf
+sed -i "s|MET_INSTALL_DIR = /path/to|MET_INSTALL_DIR = $HOME/WRF/MET-10.1.2|" defaults.conf
+sed -i "s|INPUT_BASE = /path/to|INPUT_BASE = $HOME/WRF/METplus-4.1.2/Sample_Data|" defaults.conf
+sed -i "s|OUTPUT_BASE = /path/to|OUTPUT_BASE = $HOME/WRF/METplus-4.1.2/Output|" defaults.conf
 
 
 # Downloading Sample Data
 
-cd $HOME/WRF-Hydro/METplus-4.1.2/Downloads
+cd $HOME/WRF/METplus-4.1.2/Downloads
 wget https://dtcenter.ucar.edu/dfiles/code/METplus/METplus_Data/v4.1/sample_data-met_tool_wrapper-4.1.tgz
-tar -xvzf sample_data-met_tool_wrapper-4.1.tgz -C $HOME/WRF-Hydro/METplus-4.1.2/Sample_Data
+tar -xvzf sample_data-met_tool_wrapper-4.1.tgz -C $HOME/WRF/METplus-4.1.2/Sample_Data
 
 
 # Testing if installation of MET & METPlus was sucessfull
@@ -45,8 +45,8 @@ tar -xvzf sample_data-met_tool_wrapper-4.1.tgz -C $HOME/WRF-Hydro/METplus-4.1.2/
 # Then MET & METPLUS is sucessfully installed
 
 echo 'Testing MET & METPLUS Installation.'
-$HOME/WRF-Hydro/METplus-4.1.2/ush/run_metplus.py -c $HOME/WRF-Hydro/METplus-4.1.2/parm/use_cases/met_tool_wrapper/GridStat/GridStat.conf 
+$HOME/WRF/METplus-4.1.2/ush/run_metplus.py -c $HOME/WRF/METplus-4.1.2/parm/use_cases/met_tool_wrapper/GridStat/GridStat.conf 
 
-export PATH=$HOME/WRF-Hydro/METplus-4.1.2/ush:$PATH
+export PATH=$HOME/WRF/METplus-4.1.2/ush:$PATH
 
 
